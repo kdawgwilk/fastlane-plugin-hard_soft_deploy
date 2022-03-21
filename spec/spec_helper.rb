79a7d78ad45e8a1b5c9e89a533b87d3a21de4f97
Kaden Wilkinson <7799267+kdawgwilk@users.noreply.github.com>
2022-03-21 10:02:50 -0600

    Add git dep (#2)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ $LOAD_PATH.unshift(File.expand_path('../lib', __dir__))
 
 require 'simplecov'
 
-# SimpleCov.minimum_coverage 95
+SimpleCov.minimum_coverage(80)
 SimpleCov.start
 
 # This module is only used to check the environment is currently a testing env
